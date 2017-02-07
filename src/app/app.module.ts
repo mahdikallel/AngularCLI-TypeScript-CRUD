fb2fc317047de44fb133a615e94745cbdd116a36
Mahdi Kallel <Mahdi Kallel>
2017-02-07 17:26:21 +0100

    adding ng2 bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import {Routes, RouterModule} from "@angular/router";
+import { DesignComponent } from './design/design.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 
 
 const routes: Routes = [
-  { path: ':status', component: TodoComponent },
-  { path: '**', redirectTo: '/all' } // otherwise **
+  { path: 'todo/:status', component: TodoComponent },
+  { path: 'design', component:DesignComponent },
+  { path: '**', component: PageNotFoundComponentComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    DesignComponent,
+    PageNotFoundComponentComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
