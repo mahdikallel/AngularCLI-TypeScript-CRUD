e8cad2007b22d04428fdd245c9fd5f5f115de5a1
Mahdi Kallel <Mahdi Kallel>
2017-02-07 18:13:43 +0100

    prob Modal bootstrap

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalDirective } from 'ng2-bootstrap';
 @Component({
   selector: 'app-design',
   templateUrl: 'design.component.html',
-  styleUrls: ['design.component.css']
+  styleUrls: ['design.component.css'],
 })
 export class DesignComponent implements OnInit {
 
   constructor() { }
 
+  staticModal(){
+    console.log("Modal");
+  }
   ngOnInit() {
+
   }
 
 }
